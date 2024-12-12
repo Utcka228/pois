ccbb9eace139ac1ad993658625a4df62a9ac9ece
Utcka228 <137770082+Utcka228@users.noreply.github.com>
2024-12-12 17:42:21 +0300

    Add files via upload

---
+++ dbed3.php
@@ -0,0 +1,36 @@
+<?php 
+if (isset($_POST['name']) && isset($_POST['phone'])  && isset($_POST['tg'])){
+    // Переменные с формы
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $tg = $_POST['tg'];
+    
+    // Параметры для подключения
+    $db_host = "localhost"; 
+    $db_user = "matveysh_228"; // Логин БД
+    $db_password = "123ert678IOP"; // Пароль БД
+    $db_base = 'matveysh_228'; // Имя БД
+    $db_table = "konstruktor"; // Имя Таблицы БД
+    
+    try {
+        // Подключение к базе данных
+        $db = new PDO("mysql:host=$db_host;dbname=$db_base", $db_user, $db_password);
+        // Устанавливаем корректную кодировку
+        $db->exec("set names utf8");
+        // Собираем данные для запроса
+        $data = array( 'name' => $name, 'phone' => $phone, 'tg' => $tg); 
+        // Подготавливаем SQL-запрос
+        $query = $db->prepare("INSERT INTO $db_table (name, phone, tg) values (:name, :phone, :tg)");
+        // Выполняем запрос с данными
+        $query->execute($data);
+        // Запишим в переменую, что запрос отрабтал
+        $result = true;
+    } catch (PDOException $e) {
+        // Если есть ошибка соединения или выполнения запроса, выводим её
+        print "Ошибка!: " . $e->getMessage() . "<br/>";
+    }
+    
+    if ($result) {
+    	header("Location: Uspesho.html");
+    }
+}
